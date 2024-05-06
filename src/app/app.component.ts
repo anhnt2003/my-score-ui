77ff182d049054671d276e2e440d71f96e7abd47
anhnt <anh.nt15@citigo.com.vn>
2024-05-06 15:09:56 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IconFieldModule, InputIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
